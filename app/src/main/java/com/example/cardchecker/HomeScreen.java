ebcd0cc79937f66b5bd7c4091529b5889f0c7ec5
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-10-10 15:35:46 +0000

    Finished the algorithm part of the project.

--- app/src/main/java/com/example/cardchecker/HomeScreen.java
+++ app/src/main/java/com/example/cardchecker/HomeScreen.java
@@ -3,12 +3,58 @@ package com.example.cardchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.cardchecker.customFunctions.HomeScreenFunctions;
 
 public class HomeScreen extends AppCompatActivity {
+
+    HomeScreenFunctions homeScreenFunctions = new HomeScreenFunctions();
+
+    //Buttons and textview
+    private TextView txtViewInputCardNo;
+    private Button btnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Mapping the elements to the xml, and setting up their onCreate functions.
+        txtViewInputCardNo = (TextView) findViewById(R.id.txtViewInputCardNo);
+        txtViewInputCardNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Some code to check if what the user typed is more equal to sixteen or otherwise?
+            }
+        });
+
+        btnCheck = (Button) findViewById(R.id.btnCheck);
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (homeScreenFunctions.ValidateNumber(txtViewInputCardNo.getText().toString())) {
+                    //If true change the view to success.
+                    System.out.println("Valid card");
+                }
+                else {
+                    //Otherwise change the view to failed.
+                    System.out.println("Not a valid card.");
+                }
+            }
+        });
+
     }
 }
+
+
+
+/*How to change a view in android java
+ buttonChangeView.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
+        startActivity(intent);
+        }*/
---
+++ app/src/main/java/com/example/cardchecker/customFunctions/HomeScreenFunctions.java
@@ -0,0 +1,43 @@
+package com.example.cardchecker.customFunctions;
+
+import android.text.method.NumberKeyListener;
+
+public class HomeScreenFunctions {
+
+    //This function checks whether the card is valid or invalid.
+    public Boolean ValidateNumber(String cardNo) {
+
+        System.out.println(cardNo);
+        //The sum of all the idNumbers after being processed. It must always be 0 when this method is called.
+        int checkSum = 0;
+        //This array is where all the card numbers will go, with each number getting a space.
+        String[] arrayOfCardNo = new String[cardNo.length()];
+
+        for (int i = 0; i < cardNo.length(); i++) {
+            //Put every number from our cardNumber into the arrayOfCardNo, with each cardNumber getting a space.
+            arrayOfCardNo[i] = String.valueOf(cardNo.charAt(i));
+
+            //Check and see if the number is at a location which is divisible by two with a reminder, because we are interested
+            //in second digits from the rightmost, thus we only want numbers which are located at odd indexes in the array.
+            if (i % 2 != 0) {
+                //When found that item is multiplied by two and stored inside the array index where it was found.
+                arrayOfCardNo[i] = String.valueOf(Integer.parseInt(arrayOfCardNo[i]) * 2);
+
+                //Check and see if the number you just stored is two numbers by using length of a string.
+                if (arrayOfCardNo[i].length() >= 2) {
+                    //If it is two numbers access the numbers separately and add them together, and store it where it was picked from.
+                    //And there is now way the number will be more than two lengths.
+                    int addTwoNo = Integer.parseInt(String.valueOf(arrayOfCardNo[i].charAt(0))) + Integer.parseInt(String.valueOf(arrayOfCardNo[i].charAt(1)));
+                    arrayOfCardNo[i] = String.valueOf(addTwoNo);
+                }
+            }
+
+            //Sum up the values
+            checkSum += Integer.parseInt(arrayOfCardNo[i]);
+        }
+
+        //Check to see if the sum of the id number is valid or not,
+        // by checking to see if the number modulo 10 is 0, then return.
+        return checkSum % 10 == 0;
+    }
+}

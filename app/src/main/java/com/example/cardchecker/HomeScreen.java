fb1d4b2087cc9782d327415dc58ff2e0d3173259
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-10-10 20:28:11 +0000

    Finished the algoritm and its funcions

--- app/src/main/java/com/example/cardchecker/HomeScreen.java
+++ app/src/main/java/com/example/cardchecker/HomeScreen.java
@@ -1,13 +1,17 @@
 package com.example.cardchecker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.cardchecker.customFunctions.HomeScreenFunctions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -35,14 +39,37 @@ public class HomeScreen extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (homeScreenFunctions.ValidateNumber(txtViewInputCardNo.getText().toString())) {
-                    //If true change the view to success.
-                    System.out.println("Valid card");
+                //Handling edge case, when the user does not input any value, it sends 0
+                // because nothing has been typed. So it needs to stop,otherwise the
+                // success page would be called, because 0%0 is 0.
+                // We can check if the textField has some input or not before calling he ValidateNumber function
+                if (!txtViewInputCardNo.getText().toString().equals("")) {
+                    if (homeScreenFunctions.ValidateNumber(txtViewInputCardNo.getText().toString())) {
+                        //If true change the view to success.
+                        System.out.println("Valid card");
+                        Intent intent = new Intent(HomeScreen.this, SuccessScreen.class);
+                        startActivity(intent);
+                    } else {
+                        //Otherwise change the view to failed.
+                        System.out.println("Not a valid card.");
+                        Intent intent = new Intent(HomeScreen.this, FailActivity.class);
+                        startActivity(intent);
+                    }
+                } else {
+                    //If the user types nothing create an alert and put it inside an AlertDialog.
+                    AlertDialog.Builder alert_TypeAText = new AlertDialog.Builder(HomeScreen.this);
+                    alert_TypeAText.setMessage(R.string.message_home_alert);
+                    alert_TypeAText.setCancelable(true);
+                    alert_TypeAText.setPositiveButton(R.string.alert_positive_button, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog alertDialog = alert_TypeAText.create();
+                    alertDialog.show();
                 }
-                else {
-                    //Otherwise change the view to failed.
-                    System.out.println("Not a valid card.");
-                }
+
             }
         });
 

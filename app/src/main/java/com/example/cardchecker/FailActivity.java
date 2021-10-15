6d008aa38d15b49a326c6204ccee84dc75b72542
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-10-15 12:02:18 +0000

    Success and failed page done, try again button working.

--- app/src/main/java/com/example/cardchecker/FailActivity.java
+++ app/src/main/java/com/example/cardchecker/FailActivity.java
@@ -3,12 +3,24 @@ package com.example.cardchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FailActivity extends AppCompatActivity {
+
+    private Button btTryAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fail);
+
+        btTryAgain = (Button) findViewById(R.id.BTFail);
+        btTryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cardchecker/SuccessScreen.java
+++ app/src/main/java/com/example/cardchecker/SuccessScreen.java
@@ -1,15 +1,27 @@
 package com.example.cardchecker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SuccessScreen extends AppCompatActivity {
 
+    private Button btTryAgain;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        btTryAgain = (Button) findViewById(R.id.BTtryAgain);
+        btTryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
